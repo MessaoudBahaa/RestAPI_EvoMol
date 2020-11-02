d178ca12f8894f80d373891cdc0d62653f5766ba
MessaoudBahaa <fm_berbache@esi.dz>
2020-11-02 18:10:04 +0100

    added routes to get outputs + updating client file to the new requests

--- api.py
+++ api.py
@@ -8,25 +8,6 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
 
 @app.route('/', methods=['GET'])
 def home():
@@ -110,13 +91,26 @@ def api_est_termine():
 
     return jsonify((data["execution"]["status"]=="TERMINATED"))
 
+# retourner si une evaluation d'un individu etait un succés
+@app.route('/api/evaluate/estsucces', methods =['POST'])
+def api_est_succes():
+    req_data = request.get_json()
+    filename = str(req_data['id'])+'.tmp'
+    with open(filename, "r") as read_file:
+        data = json.load(read_file)
+    read_file.close()
+    return jsonify((data["execution"]["output"]["result"]=="SUCCESS"))
+
+# retourner les resultats d'un evaluation d'un individu
+@app.route('/api/evaluate/output', methods =['POST'])
+def api_get_evaluation_data():
+    req_data = request.get_json()
+    filename = str(req_data['id'])+'.tmp'
+    with open(filename, "r") as read_file:
+        data = json.load(read_file)
+    read_file.close()
+    return jsonify((data["execution"]["output"])
 
 # execution: RUNNING / TERMINATED 
 # output status: ERROR / SUCCESS and results  
 
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
-
-app.run()
--- client.py
+++ client.py
@@ -6,4 +6,9 @@ payload = {'id':2,'bits':'0100101110101'}
 
 r = requests.post('http://127.0.0.1:5000/api/evaluate', json=payload)
 
+#r = requests.post('http://127.0.0.1:5000/api/evaluate/status', json=payload)
+#r = requests.post('http://127.0.0.1:5000/api/evaluate/esttermine', json=payload)
+#r = requests.post('http://127.0.0.1:5000/api/evaluate/estsucces', json=payload)
+#r = requests.post('http://127.0.0.1:5000/api/evaluate/output', json=payload)
+
 print (r.text)
